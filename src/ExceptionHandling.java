689bec8e20e568381e29b79ce3f51184d06631cd
Sahil Shrestha <35665722+sahilian@users.noreply.github.com>
2024-01-23 23:49:52 +0545

    Exception Handling

--- src/ExceptionHandling.java
+++ src/ExceptionHandling.java
@@ -1,11 +1,16 @@
 public class ExceptionHandling {
-    public static void main(String[] args) {
-        try {
+    public static void main(String[] args)
+    {
+        try
+        {
             int a = 10;
             int b = 0;
             int c = a / b;
             System.out.println(c);
-        } catch (ArithmeticException e) {
+        }
+
+        catch (ArithmeticException e)
+        {
             System.out.println("Divide By Zero Exception");
         }
     }
