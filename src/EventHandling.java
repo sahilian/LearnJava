dee1a6ee3db85a5c5b576399f16615c9c0217d5c
Sahil Shrestha <35665722+sahilian@users.noreply.github.com>
2024-01-25 12:46:09 +0545

    Event Listner

---
+++ src/EventHandling.java
@@ -0,0 +1,22 @@
+import java.awt.event.*;
+import javax.swing.*;
+
+public class EventHandling extends JFrame implements ActionListener {
+    public EventHandling() {
+        JButton btn = new JButton("Click Me");
+        add(btn);
+        btn.addActionListener(this);         //registering the event handler
+        setSize(100, 100);
+        setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        JOptionPane.showMessageDialog(null, "Hello");
+    }
+
+    public static void main(String[] a) {
+        new EventHandling();
+    }
+}
