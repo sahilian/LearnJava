82c6d6e63f38ab384449621f431d73fbe461204d
Sahil Shrestha <35665722+sahilian@users.noreply.github.com>
2024-02-06 21:08:04 +0545

    file handling

---
+++ src/FileHandling/ByteStream.java
@@ -0,0 +1,27 @@
+package FileHandling;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
+public class ByteStream {
+    public static void main(String[] args) throws Exception {
+        FileInputStream in = null;
+        FileOutputStream out = null;
+        try {
+            in = new FileInputStream("src\\FileHandling\\Input.txt");
+            out = new FileOutputStream("src\\FileHandling\\Output.txt");
+            int c;
+            while ((c = in.read()) != -1) {
+                out.write(c);
+            }
+        } finally {
+            // Close streams only if they are not null
+            if (in != null) {
+                in.close();
+            }
+            if (out != null) {
+                out.close();
+            }
+        }
+    }
+}
