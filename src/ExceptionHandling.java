1e073894256c02db2a4610f255ab032325d04d03
Sahil Shrestha <35665722+sahilian@users.noreply.github.com>
2024-02-06 15:25:47 +0545

    custom exception

--- src/ExceptionHandling.java
+++ src/ExceptionHandling.java
@@ -48,9 +48,15 @@ class MyCustomException extends Exception {
 class MyExceptionTester {
     static int a = -10;
 
-    public static void main(String[] args) throws MyCustomException {
+    public static void main(String[] args) {
         if (a < 0) {
-            throw new MyCustomException();
+            try {
+                throw new MyCustomException();
+            }
+            catch (MyCustomException e)
+            {
+                System.out.println(e);
+            }
         } else {
             System.out.println("No Exception!");
         }
