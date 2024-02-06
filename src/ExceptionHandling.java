069e412fe3003a1a8ab3e604d043d053eb4c08a2
Sahil Shrestha <35665722+sahilian@users.noreply.github.com>
2024-02-06 14:02:57 +0545

    custom exception

--- src/ExceptionHandling.java
+++ src/ExceptionHandling.java
@@ -1,39 +1,58 @@
 public class ExceptionHandling {
-    public static void main(String[] args)
-    {
-        try
-        {
+    public static void main(String[] args) {
+        try {
             int a = 10;
             int b = 0;
             int c = a / b;
             System.out.println(c);
-        }
-
-        catch (ArithmeticException e)
-        {
-            System.out.println("Divide By Zero Exception "+e);
+        } catch (ArithmeticException e) {
+            System.out.println("Divide By Zero Exception " + e);
         }
     }
 }
 
-class ArrayException
-{
-    public static void main(String[] args)
-    {
-        try
-        {
-            int A[] = {10,0,2,6,7};
-            int r = A[0]/A[2];
+class ArrayException {
+    public static void main(String[] args) {
+        try {
+            int A[] = {10, 0, 2, 6, 7};
+            int r = A[0] / A[2];
             System.out.println(r);
             System.out.println(A[10]);
+        } catch (ArithmeticException e) {
+            System.out.println(e);
+        } catch (ArrayIndexOutOfBoundsException ex) {
+            System.out.println(ex);
         }
-        catch (ArithmeticException e)
-        {
+
+    }
+}
+
+class TryCatchThrow {
+    public static void main(String[] args) {
+        try {
+            int a = 10 / 0;
+        } catch (Exception e) {
             System.out.println(e);
         }
-        catch (ArrayIndexOutOfBoundsException ex)
-        {
-            System.out.println(ex);
+        System.out.println("Hi!");
+
+    }
+}
+
+class MyCustomException extends Exception {
+    public String toString() {
+        return "My exception called";
+    }
+}
+
+class MyExceptionTester {
+    static int a = -10;
+
+    public static void main(String[] args) throws MyCustomException {
+        if (a < 0) {
+            throw new MyCustomException();
+        } else {
+            System.out.println("No Exception!");
         }
 
     }
