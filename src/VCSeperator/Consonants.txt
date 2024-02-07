368ff5dfd08bda7a7af9b764b007bee7a7ec8cf2
Sahil Shrestha <35665722+sahilian@users.noreply.github.com>
2024-02-07 11:23:40 +0545

    vowel consonants seperator

---
+++ src/VCSeperator/Consonants.txt
@@ -0,0 +1,1 @@
+hllwrld
---
+++ src/VCSeperator/Seperator.java
@@ -0,0 +1,45 @@
+package VCSeperator;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.Scanner;
+
+public class Seperator {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter your sentence: ");
+        String input = scan.nextLine().toLowerCase();
+        String vowels = "";
+        String consonants = "";
+        for(char c : input.toCharArray())
+        {
+            if(c=='a' ||c=='e' ||c=='i' ||c=='o' ||c=='u')
+            {
+                vowels += c;
+            }
+            else if (Character.isLetter(c)) {
+                consonants += c;
+            }
+        }
+        try
+        {
+            FileWriter consonantsWriter = new FileWriter("src/VCSeperator/Consonants.txt");
+            consonantsWriter.write(consonants.toString());
+            consonantsWriter.close();
+        }
+        catch (Exception e)
+        {
+            System.out.println(e);
+        }
+        try
+        {
+            FileWriter vowelWriter = new FileWriter("src/VCSeperator/Vowels.txt");
+            vowelWriter.write(vowels.toString());
+            vowelWriter.close();
+        }
+        catch (Exception e)
+        {
+            System.out.println(e);
+        }
+    }
+}
